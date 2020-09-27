2985f0272e6c3f5947447c2a3eff27b3af8d212a
Douglas Naphas <douglasnaphas@gmail.com>
2020-09-27 10:36:00 -0400

    Publish a publicly-available API endpoint
    https://cdkworkshop.com/20-typescript/30-hello-cdk/300-apigw.html

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,21 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // define an AWS lambda resource
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X, // execution environment
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // define an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
