a567688a2b39e2484f73b65a8c6d9f8fa4d2f1b9
Douglas Naphas <douglasnaphas@gmail.com>
2020-09-27 23:05:43 -0400

    Implement a hit counter with dynamo
    This completes the CDK Workshop tutorial through https://cdkworkshop.com/20-typescript/40-hit-counter/700-test.html

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -1,4 +1,4 @@
-exports.handler = async function (event: { path: any }) {
+exports.handler = async function (event: { path: string }) {
   console.log("request:", JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
